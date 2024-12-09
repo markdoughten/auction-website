145991dcf5954328b85a5e46343ff0f0a4a2cd28
Krishna Sathyamurthy <64264567+krshsl@users.noreply.github.com>
2024-12-08 20:19:54 -0500

    new item template added

--- src/frontend/src/app/layouts/items/itemform/itemform.component.ts
+++ src/frontend/src/app/layouts/items/itemform/itemform.component.ts
@@ -1,10 +1,38 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { IS_REQUIRED } from "@core/constants";
 
 @Component({
   selector: "app-newitem",
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: "./itemform.component.html",
   styleUrl: "./itemform.component.css",
 })
-export class ItemFormComponent {}
+export class ItemFormComponent {
+  itemForm: FormGroup;
+  readonly IS_REQUIRED = IS_REQUIRED;
+  MIN_VALUE = "Minimum value should be greater than 0";
+
+  constructor(private fb: FormBuilder) {
+    this.itemForm = this.fb.group({
+      name: ["", Validators.required],
+      description: [""],
+      price: [0, Validators.min(0)],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.itemForm);
+    if (this.itemForm.valid) {
+      // Process the form data, e.g., send it to a backend service
+      console.log(this.itemForm.value);
+    }
+  }
+}
